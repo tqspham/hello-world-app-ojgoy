55a4b91479bdb51da5905d74f14e269cda1a6e61
tqspham <tqs.pham@gmail.com>
2026-05-16 23:17:49 +0200

    chore: hello world app...

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-white">
+      <h1 className="text-4xl font-bold text-gray-900">Hello, World!</h1>
+    </main>
+  );
+}
